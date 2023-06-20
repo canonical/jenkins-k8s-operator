5c86deefe6a32e66d990c5d1276fc28fecb046ae
charlie4284 <yangsoo.yoon@canonical.com>
2023-06-20 23:50:34 +0800

    test: add more invalid test cases

--- tests/unit/test_state.py
+++ tests/unit/test_state.py
@@ -17,6 +17,10 @@ import state
         pytest.param("00-24", id="Out of 24H scale"),
         pytest.param("23", id="Not a range"),
         pytest.param("23-23", id="Same time given as range"),
+        pytest.param("00:01-00:02", id="Hour:minute format"),
+        pytest.param("3PM-4PM", id="Non 24H format"),
+        pytest.param("01--02", id="Invalid input (extra dash)"),
+        pytest.param("2023‐06‐20T15:31:23Z-2023‐09‐20T15:31:23Z", id="ISO timestamp range"),
     ],
 )
 def test_update_time_range_invalid_time(time_range: str):
