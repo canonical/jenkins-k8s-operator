69eed195baeecfb08eda7f53afeb296d0d4fddfe
charlie4284 <yangsoo.yoon@canonical.com>
2023-08-16 15:27:09 +0800

    test: integration test initial commit

--- tests/integration/types_.py
+++ tests/integration/types_.py
@@ -4,7 +4,9 @@
 """Types for integration tests module."""
 
 import dataclasses
+import typing
 
+import jenkinsapi.jenkins
 from juju.application import Application
 from juju.model import Model
 from juju.unit import Unit
@@ -23,3 +25,33 @@ class ModelAppUnit:
     model: Model
     app: Application
     unit: Unit
+
+
+@dataclasses.dataclass
+class UnitWebClient:
+    """The unit, web address, jenkins client wrapper dataclass.
+
+    Attributes:
+        unit: The jenkins application unit.
+        web: The jenkins unit web address.
+        client: The client connected to jenkins unit.
+    """
+
+    unit: Unit
+    web: str
+    client: jenkinsapi.jenkins.Jenkins
+
+
+@dataclasses.dataclass
+class PluginsMeta:
+    """The plugin config, plugin to install, plugin to remove wrapper dataclass.
+
+    Attributes:
+        config: The plugins applied on charm config.
+        install: The plugins to be installed on jenkins.
+        remove: The plugins that should be removed.
+    """
+
+    config: typing.Iterable[str]
+    install: typing.Iterable[str]
+    remove: typing.Iterable[str]
