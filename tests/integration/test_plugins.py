cc2233cb4627b2770881e8038c90d8789e184d80
charlie4284 <yangsoo.yoon@canonical.com>
2023-08-30 15:46:46 +0800

    fix: typo in fixture access

--- tests/integration/test_plugins.py
+++ tests/integration/test_plugins.py
@@ -72,7 +72,7 @@ async def test_git_plugin_k8s_agent(
 
     # check that git plugin git repository validation works on Jenkins server
     check_url_res = unit_web_client.client.requester.post_url(
-        f"{jenkins_cunit_web_client.clientlient.baseurl}/job/{job_name}/descriptorByName/"
+        f"{unit_web_client.clientlient.baseurl}/job/{job_name}/descriptorByName/"
         "hudson.plugins.git.UserRemoteConfig/checkUrl",
         data={
             "value": "https://github.com/canonical/jenkins-k8s-operator",
