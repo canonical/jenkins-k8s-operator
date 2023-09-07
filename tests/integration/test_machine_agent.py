083cbffcfc3403277f0fce273f5d279834c623ec
Yanks Yoon <37652070+yanksyoon@users.noreply.github.com>
2023-09-07 13:47:57 +0800

    test: Git plugin integration test (#27)
    * fix: typo in template
    
    * chore: change path to jenkins
    
    * chore: remove debug logs
    
    * chore: refactor tests
    
    * test: fix wait for machine agent
    
    * test: finally working tests w/ ops
    
    * Revert "fix: typo in template"
    
    This reverts commit fc9b6bd592a7d7a54c884c76af47fd425125e0ca.
    
    * chore: use edge jenkins-agent-k8s
    
    * chore: add docstrings to fixtures
    
    * chore: remove application cleanup
    
    * chore: use fixture for setup
    
    * fix: break line-too-long line
    
    * debug
    
    * fix: add git to jenkins-k8s container
    
    * debug: test_jenkins.py
    
    * debug: test_agent.py
    
    * chore: try function scoped jenkins client
    
    * debug: test_plugins.py
    
    * fix: fixture scoping
    
    * trigger ci
    
    * chore: test all
    
    * test: test by modules
    
    * test: fix modules
    
    * test: try model relate
    
    * chore: refactor plugin fixtures to constants
    
    * chore: refactor install plugins to helper
    
    * fix: typo in fixture access
    
    * test: install plugins helper with restart
    
    * test: apply update install plugins
    
    * test: fix linting
    
    * test: merge setup/teardown fixtures
    
    * test: linting fixes
    
    * test: fix wait for idle on blocked agents
    
    * test: deprecated agent relation remove
    
    * test: fix destroy relation
    
    * test: fix destroy relation
    
    * debug
    
    * test: try await model connect for websocket issue
    
    * test: duplicate code lint fix
    
    * test: model connect define controller & user
    
    * test: print stdout and stderr
    
    * test: remove tmate debug
    
    * test: read from stderr for plugins install exec
    
    * test: wait for idle and increase check frequency
    
    * test: reduce jenkins units to 2
    
    * test: try remove relation by app name
    
    * test: revert stderr and relation name
    
    * test: check for done in both stdout and stderr
    
    * test: refactor destroy relation remote application
    
    * test: try connecting to model before destroy relation
    
    * test: try limit 2 & no cleanup
    
    * Revert "test: try limit 2 & no cleanup"
    
    This reverts commit b6b2671f6bcba3276dfd979cdad577d217b6366f.
    
    * test: increase limit for all relations & reorder tests
    
    * test: split agent tests (mitigate juju bug)
    
    * fix: remove application
    
    * test: refactor gen_job_xml fixtures (duplicate code lint fix)
    
    * test: remove application remove relation
    
    * test: remove try function scoped applications
    
    * test: try module scope for application
    
    * test: split machine plugins test
    
    * test: split machine-k8s agents tests
    
    * test: matrix
    
    * test: slow interval
    
    * test: mitigate use of context to prevent cleanup with juju
    
    * test: remove plugin test on machine agents
    
    * test: remove plugin test on machine agents
    
    * test: cleanup testing code
    
    * test: fix fixture app_with_allowed_plugins
    
    * test: add back model connect
    
    * test: wait before model relate
    
    * test: put back ops_test fast forward
    
    * test: reduce num units to 2
    
    * test: replace async await model with config set
    
    * Revert "test: replace async await model with config set"
    
    This reverts commit 50e5f70a70e81828261aa8497d8df184791a0f4a.
    
    * chore: address code style comments
    
    * chore: refer to model from unit
    
    * chore: revert changes to working test
    
    * debug
    
    * Revert "debug"
    
    This reverts commit 89cf4accfdb33908390c27a7e10cac2bc23ce15e.

---
+++ tests/integration/test_machine_agent.py
@@ -0,0 +1,64 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Integration tests for jenkins-k8s-operator charm."""
+
+import jenkinsapi.jenkins
+import pytest
+from juju.application import Application
+
+import state
+
+from .helpers import assert_job_success
+
+
+async def test_jenkins_machine_agent_relation(
+    application: Application,
+    jenkins_machine_agents: Application,
+    jenkins_client: jenkinsapi.jenkins.Jenkins,
+):
+    """
+    arrange: given a cross controller cross model jenkins machine agent with an offer.
+    act:
+        1. when the server charm is related to the machine agent charm.
+        2. when the relation is removed.
+    assert:
+        1. the relation succeeds and the machine agent is able to run jobs successfully.
+        2. the machine agent is deregistered from Jenkins.
+    """
+    # 1. Relate jenkins-k8s charm to the jenkins-agent charm.
+    model = application.model
+    machine_model = jenkins_machine_agents.model
+    # this code is similar to the app_machine_agent_related fixture but shouldn't be using the
+    # fixture since this test tests for teardown of relation as well.
+    # pylint: disable=duplicate-code
+    await model.relate(
+        f"{application.name}:{state.AGENT_RELATION}",
+        f"localhost:admin/{machine_model.name}.{state.AGENT_RELATION}",
+    )
+    await machine_model.wait_for_idle(apps=[jenkins_machine_agents.name], wait_for_active=True)
+    await model.wait_for_idle(apps=[application.name], wait_for_active=True)
+    # pylint: enable=duplicate-code
+
+    # 1. Assert that the node is registered and is able to run jobs successfully.
+    assert_job_success(jenkins_client, jenkins_machine_agents.name, "machine")
+
+    # 2. Remove the relation
+    await application.remove_relation(state.AGENT_RELATION, state.AGENT_RELATION)
+    await model.wait_for_idle(apps=[application.name])
+    await machine_model.wait_for_idle(apps=[jenkins_machine_agents.name])
+
+    # 2. Assert that the agent nodes are deregistered from Jenkins.
+    assert not any((application.name in key for key in jenkins_client.get_nodes().keys()))
+
+
+@pytest.mark.usefixtures("app_machine_deprecated_agent_related")
+async def test_jenkins_machine_deprecated_agent_relation(
+    jenkins_machine_agents: Application, jenkins_client: jenkinsapi.jenkins.Jenkins
+):
+    """
+    arrange: given a cross controller cross model jenkins machine agent with an offer.
+    act: when the relation is setup through the offer.
+    assert: the relation succeeds and the agent is able to run jobs successfully.
+    """
+    assert_job_success(jenkins_client, jenkins_machine_agents.name, "machine")
--- tests/integration/test_plugins.py
+++ tests/integration/test_plugins.py
@@ -8,14 +8,15 @@ import typing
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from .types_ import PluginsMeta, UnitWebClient
+from .constants import ALLOWED_PLUGINS, INSTALLED_PLUGINS, REMOVED_PLUGINS
+from .helpers import gen_git_test_job_xml, install_plugins
+from .types_ import UnitWebClient
 
 
-@pytest.mark.usefixtures("install_plugins")
+@pytest.mark.usefixtures("app_with_allowed_plugins")
 async def test_jenkins_plugins_config(
     ops_test: OpsTest,
     unit_web_client: UnitWebClient,
-    plugins_meta: PluginsMeta,
     update_status_env: typing.Iterable[str],
 ):
     """
@@ -23,6 +24,10 @@ async def test_jenkins_plugins_config(
     act: when update_status_hook is fired.
     assert: the plugin is uninstalled and the system message is set on Jenkins.
     """
+    await install_plugins(
+        ops_test, unit_web_client.unit, unit_web_client.client, INSTALLED_PLUGINS
+    )
+
     ret_code, _, stderr = await ops_test.juju(
         "exec",
         "--unit",
@@ -34,10 +39,37 @@ async def test_jenkins_plugins_config(
     res = unit_web_client.client.requester.get_url(unit_web_client.web)
     page_content = str(res.content, encoding="utf-8")
 
-    assert all(plugin in page_content for plugin in plugins_meta.remove), page_content
+    assert all(plugin in page_content for plugin in REMOVED_PLUGINS), page_content
     assert "The following plugins have been removed by the system administrator:" in page_content
     assert (
         "To allow the plugins, please include them in the plugins configuration of the charm."
         in page_content
     )
-    assert all(unit_web_client.client.has_plugin(plugin) for plugin in plugins_meta.config)
+    assert all(unit_web_client.client.has_plugin(plugin) for plugin in ALLOWED_PLUGINS)
+
+
+@pytest.mark.usefixtures("app_k8s_agent_related")
+async def test_git_plugin_k8s_agent(ops_test: OpsTest, unit_web_client: UnitWebClient):
+    """
+    arrange: given a jenkins charm with git plugin installed.
+    act: when a job is dispatched with a git workflow.
+    assert: job completes successfully.
+    """
+    await install_plugins(
+        ops_test, unit_web_client.unit, unit_web_client.client, INSTALLED_PLUGINS
+    )
+
+    job_name = "git-plugin-test-k8s"
+    unit_web_client.client.create_job(job_name, gen_git_test_job_xml("k8s"))
+    # check that git plugin git repository validation works on Jenkins server
+    check_url_res = unit_web_client.client.requester.post_url(
+        f"{unit_web_client.client.baseurl}/job/{job_name}/descriptorByName/"
+        "hudson.plugins.git.UserRemoteConfig/checkUrl",
+        data={
+            "value": "https://github.com/canonical/jenkins-k8s-operator",
+            "credentialsId": "",
+        },
+    )
+    assert (
+        check_url_content := str(check_url_res.content, encoding="utf-8")
+    ) == "<div/>", f"Non-empty error message returned, {check_url_content}"
