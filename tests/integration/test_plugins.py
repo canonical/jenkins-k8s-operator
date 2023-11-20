b7d5afab5107ca6c2c48502f7e2c031d9d5ff18f
charlie4284 <yangsoo.yoon@canonical.com>
2023-11-20 19:47:16 +0800

    test: fix rebuilder test fail due to timing

--- tests/integration/test_plugins.py
+++ tests/integration/test_plugins.py
@@ -320,5 +320,6 @@ async def test_rebuilder_plugin(ops_test: OpsTest, unit_web_client: UnitWebClien
     unit_web_client.client.requester.get_url(
         f"{unit_web_client.web}/job/rebuild_test/lastCompletedBuild/rebuild/"
     )
+    job.get_last_build().block_until_complete()
 
     assert job.get_last_buildnumber() == 2, "Rebuild not triggered."
