05e7d2b85834ddbf3f0d8ec3108a63679b17b4e5
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-06 16:44:47 +0100

    Fix ingress test

--- tests/integration/test_ingress.py
+++ tests/integration/test_ingress.py
@@ -20,8 +20,9 @@ async def test_ingress_integration(
     """
     status = await model.get_status(filters=[ingress_related.name])
     unit = next(iter(status.applications[ingress_related.name].units))
+    address = status["applications"][app.name]["units"][unit]["address"]
     response = requests.get(
-        f"http://{unit.address}",
+        f"http://{address}",
         headers={"Host": f"{model.name}-{ingress_related.name}.{external_hostname}"},
         timeout=5,
     ).json()
