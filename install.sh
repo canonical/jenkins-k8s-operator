0189b88810e0551d0dc56f522e5a2282e0df51f8
gtrkiller <franco.forneron@canonical.com>
2024-02-07 11:48:43 -0300

    add version upgrade test

--- install.sh
+++
@@ -1,7 +0,0 @@
-juju destroy-model jenkinsdepl --destroy-storage --force -y
-juju add-model jenkinsdepl
-charmcraft pack
-juju deploy ./jenkins-k8s_ubuntu-22.04-amd64.charm --resource jenkins-image=localhost:32000/jenkins:latest
-juju deploy jenkins-agent-k8s --channel=latest/edge --num-units=3
-juju integrate jenkins-k8s:agent jenkins-agent-k8s:agent
-watch juju status
