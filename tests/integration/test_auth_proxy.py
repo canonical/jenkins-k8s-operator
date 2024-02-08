99ed08a3668728aba432eba033380755dc62bbd6
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-08 13:49:09 +0100

    basic integration tests

--- tests/integration/test_auth_proxy.py
+++ tests/integration/test_auth_proxy.py
@@ -14,7 +14,7 @@ import jenkins
 
 
 @pytest.mark.abort_on_fail
-async def test_auth_proxy_integration_retuns_not_authorized(
+async def test_auth_proxy_integration_returns_not_authorized(
     model: Model, application: Application, oathkeeper_related: Application, external_hostname: str
 ):
     """
@@ -31,3 +31,26 @@ async def test_auth_proxy_integration_retuns_not_authorized(
         timeout=5,
     )
     assert response.status_code == 401
+
+
+@pytest.mark.abort_on_fail
+async def test_auth_proxy_integration_authorized(
+    model: Model, application: Application, oathkeeper_related: Application, external_hostname: str
+):
+    """
+    arrange: deploy the Jenkins charm and establish auth_proxy relations.
+    act: send a request to the ingress in /.
+    assert: a 401 is returned.
+    """
+    status = await model.get_status(filters=[application.name])
+    unit = next(iter(status.applications[application.name].units))
+    address = status["applications"][application.name]["units"][unit]["address"]
+    response = requests.get(
+        f"http://{address}:{jenkins.WEB_PORT}",
+        headers={
+            "Host": f"{model.name}-{application.name}.{external_hostname}",
+            "X-User": "admin",
+        },
+        timeout=5,
+    )
+    assert response.status_code == 200
