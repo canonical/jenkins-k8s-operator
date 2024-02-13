10c1a73be55473e35ea847055ffa76f3160f1f0c
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-13 15:05:26 +0100

    fix bash code smells

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -9,7 +9,7 @@
 
 # The IAM bundle required metallb to be enabled 
 IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
-microk8s enable metallb:$IPADDR-$IPADDR
+microk8s enable "metallb:$IPADDR-$IPADDR"
 
 # Jenkins machine agent charm is deployed on lxd and Jenkins-k8s server charm is deployed on
 # microk8s.
