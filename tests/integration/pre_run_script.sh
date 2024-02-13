d068b3dec5bee8123f42996bc3fe07ccb8f0bc9c
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-13 14:36:51 +0100

    fix metallb setup

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -6,15 +6,18 @@
 # Pre-run script for integration test operator-workflows action.
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
+
+# The IAM bundle required metallb to be enabled 
+IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
+microk8s enable metallb:$IPADDR-$IPADDR
+
 # Jenkins machine agent charm is deployed on lxd and Jenkins-k8s server charm is deployed on
 # microk8s.
-
-TESTING_MODEL="$(juju switch)"
-
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
 sg snap_microk8s -c "microk8s status --wait-ready"
 sg snap_microk8s -c "juju bootstrap localhost localhost"
 
 echo "Switching to testing model"
+TESTING_MODEL="$(juju switch)"
 sg snap_microk8s -c "juju switch $TESTING_MODEL"
