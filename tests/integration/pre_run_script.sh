40ddb56b1bd9a60a11f2dfe9700ab1663f05c257
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-15 09:51:28 +0100

    Fix default juju controller for integration test

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -8,8 +8,8 @@
 
 
 # The IAM bundle requires metallb to be enabled 
-# IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
-# microk8s enable "metallb:$IPADDR-$IPADDR"
+IPADDR=$(ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc')
+microk8s enable "metallb:$IPADDR-$IPADDR"
 
 # Jenkins machine agent charm is deployed on lxd and Jenkins-k8s server charm is deployed on
 # microk8s.
@@ -19,8 +19,4 @@ sg snap_microk8s -c "microk8s status --wait-ready"
 sg snap_microk8s -c "juju bootstrap localhost localhost"
 
 echo "Switching to testing model"
-TESTING_MODEL="$(juju switch)"
-juju models
-juju controllers
-echo "$TESTING_MODEL"
-sg snap_microk8s -c "juju switch $TESTING_MODEL"
+sg snap_microk8s -c "juju switch microk8s/localhost"
