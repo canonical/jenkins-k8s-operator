3dffd6167ac598b2a40d6e59b6bef7d347a2d92f
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-22 10:49:47 +0100

    Fix proxy

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -16,3 +16,7 @@ sg snap_microk8s -c "juju bootstrap localhost localhost"
 TESTING_CONTROLLER="$(juju controllers --format json | jq '.controllers | with_entries(select(.key | endswith("microk8s"))) | to_entries[0] | .key')"
 echo "Switching to testing model"
 sg snap_microk8s -c "juju switch $TESTING_CONTROLLER"
+
+# Remove proxy vars as they prevent the k8s API from being reachable.
+head -n  1 /etc/environment > temp && sudo mv temp /etc/environment
+unset HTTP_PROXY HTTPS_PROXY NO_PROXY http_proxy https_proxy no_proxy
