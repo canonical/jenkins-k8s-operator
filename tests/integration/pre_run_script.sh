97861c3a356e5f5e04a7e5432232af1ae07a9737
Arturo Seijas <arturo.seijas@canonical.com>
2024-02-22 09:44:38 +0100

    Fix metallb

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -6,10 +6,6 @@
 # Pre-run script for integration test operator-workflows action.
 # https://github.com/canonical/operator-workflows/blob/main/.github/workflows/integration_test.yaml
 
-
-# The IAM bundle requires metallb to be enabled and the tests require 3 IPs
-sg snap_microk8s -c "microk8s enable metallb:10.15.119.2-10.15.119.4"
-
 # Jenkins machine agent charm is deployed on lxd and Jenkins-k8s server charm is deployed on
 # microk8s.
 # lxd should be install and init by a previous step in integration test action.
