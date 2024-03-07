4fe54c9ea597a5c5cfef07dbcb4c93e4b254cd8a
Arturo Seijas <arturo.seijas@canonical.com>
2024-03-07 17:19:44 +0100

    Revert "Add integration test for path routing"
    This reverts commit 0bbebf10811bd765a239b7fbb7f65e4839c6fe04.

--- tests/integration/test_ingress.py
+++ tests/integration/test_ingress.py
@@ -18,10 +18,10 @@ from charm import AGENT_DISCOVERY_INGRESS_RELATION_NAME
 
 
 @pytest.mark.abort_on_fail
-async def test_ingress_integration_subdomain(
+async def test_ingress_integration(
     model: Model,
     application: Application,
-    traefik_application_subdomain: typing.Tuple[Application, str],
+    traefik_application_and_unit_ip: typing.Tuple[Application, str],
     external_hostname: str,
 ):
     """
@@ -29,7 +29,7 @@ async def test_ingress_integration_subdomain(
     act: send a request to the ingress in /.
     assert: the response succeeds.
     """
-    traefik_application, traefik_address = traefik_application_subdomain
+    traefik_application, traefik_address = traefik_application_and_unit_ip
     await application.relate("ingress", traefik_application.name)
     await model.wait_for_idle(
         apps=[application.name, traefik_application.name], wait_for_active=True, timeout=20 * 60
@@ -37,32 +37,6 @@ async def test_ingress_integration_subdomain(
     response = requests.get(
         f"http://{traefik_address}/{jenkins.LOGIN_PATH}",
         headers={"Host": f"{model.name}-{application.name}.{external_hostname}"},
-        timeout=5,
-    )
-
-    assert "Authentication required" in str(response.content)
-
-
-@pytest.mark.abort_on_fail
-async def test_ingress_integration_path(
-    model: Model,
-    application: Application,
-    traefik_application_path: typing.Tuple[Application, str],
-    external_hostname: str,
-):
-    """
-    arrange: deploy the Jenkins charm and establish relations via ingress.
-    act: send a request to the ingress in /.
-    assert: the response succeeds.
-    """
-    traefik_application, traefik_address = traefik_application_path
-    await application.relate("ingress", traefik_application.name)
-    await model.wait_for_idle(
-        apps=[application.name, traefik_application.name], wait_for_active=True, timeout=20 * 60
-    )
-    response = requests.get(
-        f"http://{traefik_address}/{model.name}-{application.name}{jenkins.LOGIN_PATH}",
-        headers={"Host": f"{external_hostname}"},
         timeout=5,
     )
 
