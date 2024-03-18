c879c1ecb628f7320fff16a2d6bda8dd1698231b
Arturo Seijas <arturo.seijas@canonical.com>
2024-03-18 09:04:39 +0100

    Install pywright deps

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -18,3 +18,7 @@ sg snap_microk8s -c "juju bootstrap localhost localhost"
 
 echo "Switching to testing model"
 sg snap_microk8s -c "juju switch $TESTING_MODEL"
+
+# Install playwright dependencies
+playwright install
+playwright install-deps
