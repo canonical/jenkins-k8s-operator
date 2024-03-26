78101f2d4c0bfab350163d45d69aed50624c4787
charlie4284 <yangsoo.yoon@canonical.com>
2024-03-26 18:54:07 +0800

    modified

--- tests/integration/test_backups.py
+++ tests/integration/test_backups.py
@@ -43,48 +43,48 @@ def microceph():
     if not os.environ.get("CI") == "true":
         raise Exception("Not running on CI. Skipping microceph installation")
 
-    for _ in range(0, 50):
-        try:
-            subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
-            subprocess.run(["sudo", "microceph", "cluster", "bootstrap"], check=True)
-            subprocess.run(["sudo", "microceph", "disk", "add", "loop,4G,3"], check=True)
-            subprocess.run(["sudo", "microceph", "enable", "rgw"], check=True)
-            output = subprocess.run(
-                [
-                    "sudo",
-                    "microceph.radosgw-admin",
-                    "user",
-                    "create",
-                    "--uid",
-                    "test",
-                    "--display-name",
-                    "test",
-                ],
-                capture_output=True,
-                check=True,
-                encoding="utf-8",
-            ).stdout
-            key = json.loads(output)["keys"][0]
-            key_id = key["access_key"]
-            secret_key = key["secret_key"]
-            boto3.client(
-                "s3",
-                endpoint_url="http://localhost",
-                aws_access_key_id=key_id,
-                aws_secret_access_key=secret_key,
-            ).create_bucket(Bucket=_BUCKET)
-        except EndpointConnectionError as exc:
-            logger.error(exc)
-            logger.info(
-                "Microceph status: %s", subprocess.run(["sudo", "microceph", "status"], check=True)
-            )
-            logger.info(
-                "Microceph status: %s",
-                subprocess.run(["sudo", "microceph.radosgw-admin", "user", "check"], check=True),
-            )
-            raise
-        else:
-            subprocess.run(["sudo", "snap", "remove", "microceph", "--purge"], check=True)
+    try:
+        subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
+        subprocess.run(["sudo", "microceph", "cluster", "bootstrap"], check=True)
+        subprocess.run(["sudo", "microceph", "disk", "add", "loop,4G,3"], check=True)
+        subprocess.run(["sudo", "microceph", "enable", "rgw"], check=True)
+        output = subprocess.run(
+            [
+                "sudo",
+                "microceph.radosgw-admin",
+                "user",
+                "create",
+                "--uid",
+                "test",
+                "--display-name",
+                "test",
+            ],
+            capture_output=True,
+            check=True,
+            encoding="utf-8",
+        ).stdout
+        key = json.loads(output)["keys"][0]
+        key_id = key["access_key"]
+        secret_key = key["secret_key"]
+        boto3.client(
+            "s3",
+            endpoint_url="http://localhost",
+            aws_access_key_id=key_id,
+            aws_secret_access_key=secret_key,
+        ).create_bucket(Bucket=_BUCKET)
+    except EndpointConnectionError as exc:
+        logger.error(exc)
+        logger.info(
+            "Microceph status: %s", subprocess.run(["sudo", "microceph", "status"], check=True)
+        )
+        logger.info(
+            "Microceph status: %s",
+            subprocess.run(["sudo", "microceph.radosgw-admin", "user", "check"], check=True),
+        )
+        raise
+    else:
+        subprocess.run(["sudo", "snap", "remove", "microceph", "--purge"], check=True)
+    return ConnectionInformation(key_id, secret_key, _BUCKET)
 
 
 _BUCKET = "testbucket"
