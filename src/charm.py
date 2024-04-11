c2a27af761fada72d5797d917d2e52390dd55fe7
charlie4284 <yangsoo.yoon@canonical.com>
2024-04-11 17:41:38 +0900

    trigger

--- src/charm.py
+++ src/charm.py
@@ -53,8 +53,6 @@ class JenkinsK8sOperatorCharm(ops.CharmBase):
             return
         except CharmRelationDataInvalidError as exc:
             raise RuntimeError("Invalid relation data received.") from exc
-        
-        print("hello world")
 
         # Ingress dedicated to agent discovery
         self.agent_discovery_ingress_observer = ingress.Observer(
