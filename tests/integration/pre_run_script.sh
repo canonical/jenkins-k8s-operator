355ca54ba150dc3eecf2bdbe44cd277521135804
charlie4284 <yangsoo.yoon@canonical.com>
2025-09-04 13:32:09 +0000

    metallb ip range static

--- tests/integration/pre_run_script.sh
+++ tests/integration/pre_run_script.sh
@@ -13,9 +13,8 @@ TESTING_MODEL="$(juju switch)"
 
 # lxd should be install and init by a previous step in integration test action.
 echo "bootstrapping lxd juju controller"
-IPADDR="$( { ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc'; } );"
-export IPADDR
-sg snap_microk8s -c "microk8s enalble metallb:$IPADDR-$IPADDR"
+# Assign random IP address - this should have a very low collision rate
+sg snap_microk8s -c "microk8s enalble metallb:10.15.119.2-10.15.119.4"
 sg snap_microk8s -c "microk8s status --wait-ready --timeout 1200"
 sg snap_microk8s -c "juju bootstrap localhost localhost"
 
