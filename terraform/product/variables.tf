f3129ca4a5b6b02e80374b7ec71f81017027d31d
charlie4284 <yangsoo.yoon@canonical.com>
2025-09-29 10:30:37 +0000

    feat: terraform plan w/ oauth2-proxy, ingress, certs

--- terraform/product/variables.tf
+++ terraform/product/variables.tf
@@ -13,7 +13,7 @@ variable "jenkins_agent_k8s" {
     config      = optional(map(string), {})
     constraints = optional(string, "")
     revision    = optional(number)
-    base        = optional(string, "ubuntu@24.04")
+    base        = optional(string, "ubuntu@22.04")
     units       = optional(number, 3)
   })
 }
@@ -25,6 +25,80 @@ variable "jenkins_k8s" {
     config      = optional(map(string), {})
     constraints = optional(string, "")
     revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+  })
+}
+
+variable "public_ingress" {
+  type = object({
+    app_name = optional(string, "public-traefik-k8s")
+    channel  = optional(string, "latest/edge")
+    config = optional(map(string), {
+      "enable_experimental_forward_auth" : "true",
+      "external_hostname" : ""
+    })
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@20.04")
+  })
+}
+
+variable "agent_discovery_ingress" {
+  type = object({
+    app_name    = optional(string, "agent-discovery-traefik-k8s")
+    channel     = optional(string, "latest/edge")
+    config      = optional(map(string), {})
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@20.04")
+  })
+}
+
+variable "oauth2_proxy" {
+  type = object({
+    app_name    = optional(string, "oauth2-proxy-k8s")
+    channel     = optional(string, "latest/edge")
+    config      = optional(map(string), {})
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+  })
+}
+
+variable "httprequest_lego_k8s" {
+  type = object({
+    app_name = optional(string, "httprequest-lego-k8s")
+    channel  = optional(string, "latest/edge")
+    config = optional(map(string), {
+      "email" : "",
+      "httpreq_endpoint" : "",
+      "httpreq_http_timeout" : "180",
+      "httpreq_password" : "",
+      "httpreq_propagation_timeout" : "600"
+      "httpreq_username" : ""
+    })
+    constraints = optional(string, "")
+    revision    = optional(number)
     base        = optional(string, "ubuntu@24.04")
   })
+}
+
+variable "self_signed_ceritificates" {
+  type = object({
+    app_name    = optional(string, "self-signed-certificates")
+    channel     = optional(string, "1/stable")
+    config      = optional(map(string), {})
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@24.04")
+  })
+}
+
+variable "use_httprequest_lego_k8s_certificates" {
+  type        = bool
+  default     = false
+  description = <<EOT
+  Whether to use httprequest lego k8s to receive ceritificates.
+  Uses self-signed-certificates by default otherwise.
+  EOT
 }
