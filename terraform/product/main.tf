44f944d75262c0e393bbb7a6843f82fdc29d6cd0
Yanks Yoon <37652070+yanksyoon@users.noreply.github.com>
2025-10-06 13:44:05 +0800

    feat: terraform product support (ingress, certificates, oauth2-proxy) (#283)
    * feat: terraform plan w/ oauth2-proxy, ingress, certs
    
    * docs: terraform product
    
    * chore: remove auth proxy saas integration (not yet supported by IDP)
    
    * chore: update charm bases to noble
    
    * chore: add default values for deployment
    
    * fix: add dependency graph for stepped deployment
    
    * fix: wait for application separately

--- terraform/product/main.tf
+++ terraform/product/main.tf
@@ -28,8 +28,162 @@ module "jenkins_agent_k8s" {
   units       = var.jenkins_agent_k8s.units
 }
 
+resource "null_resource" "wait_for_jenkins_up" {
+  provisioner "local-exec" {
+    command = "juju wait-for application ${module.jenkins_k8s.app_name}"
+  }
+}
+
+
+resource "juju_application" "public_ingress" {
+  name  = var.public_ingress.app_name
+  model = var.model
+
+  charm {
+    name     = "traefik-k8s"
+    channel  = var.public_ingress.channel
+    revision = var.public_ingress.revision
+    base     = var.public_ingress.base
+  }
+
+  config      = var.public_ingress.config
+  constraints = var.public_ingress.constraints
+  units       = 1
+
+  trust = true
+}
+
+resource "juju_integration" "jenkins_k8s_public_ingress" {
+  model = var.model
+
+  depends_on = [null_resource.wait_for_jenkins_up]
+
+  application {
+    name     = module.jenkins_k8s.app_name
+    endpoint = module.jenkins_k8s.requires.ingress
+  }
+
+  application {
+    name     = juju_application.public_ingress.name
+    endpoint = "ingress"
+  }
+}
+
+resource "juju_application" "agent_discovery_ingress" {
+  name  = var.agent_discovery_ingress.app_name
+  model = var.model
+
+  charm {
+    name     = "traefik-k8s"
+    channel  = var.agent_discovery_ingress.channel
+    revision = var.agent_discovery_ingress.revision
+    base     = var.agent_discovery_ingress.base
+  }
+
+  config      = var.agent_discovery_ingress.config
+  constraints = var.agent_discovery_ingress.constraints
+  units       = 1
+
+  trust = true
+}
+
+resource "juju_integration" "jenkins_k8s_agent_discovery_ingress" {
+  model = var.model
+
+  depends_on = [null_resource.wait_for_jenkins_up]
+
+  application {
+    name     = module.jenkins_k8s.app_name
+    endpoint = module.jenkins_k8s.requires.agent_discovery_ingress
+  }
+  application {
+    name     = juju_application.agent_discovery_ingress.name
+    endpoint = "ingress"
+  }
+}
+
+resource "juju_application" "oauth2_proxy_k8s" {
+  name  = var.oauth2_proxy.app_name
+  model = var.model
+
+  charm {
+    name     = "oauth2-proxy-k8s"
+    channel  = var.oauth2_proxy.channel
+    revision = var.oauth2_proxy.revision
+    base     = var.oauth2_proxy.base
+  }
+
+  config      = var.oauth2_proxy.config
+  constraints = var.oauth2_proxy.constraints
+  units       = 1
+}
+
+resource "juju_integration" "jenkins_k8s_oauth2_proxy_k8s" {
+  model = var.model
+
+  depends_on = [null_resource.wait_for_jenkins_up]
+
+  application {
+    name     = module.jenkins_k8s.app_name
+    endpoint = module.jenkins_k8s.requires.auth_proxy
+  }
+  application {
+    name     = juju_application.oauth2_proxy_k8s.name
+    endpoint = "auth-proxy"
+  }
+}
+
+resource "juju_integration" "public_ingress_oauth2_proxy_k8s" {
+  model = var.model
+  application {
+    name     = juju_application.public_ingress.name
+    endpoint = "ingress"
+  }
+  application {
+    name     = juju_application.oauth2_proxy_k8s.name
+    endpoint = "ingress"
+  }
+}
+
+resource "juju_application" "certificate_provider" {
+  name  = var.use_httprequest_lego_k8s_certificates ? var.httprequest_lego_k8s.app_name : var.self_signed_ceritificates.app_name
+  model = var.model
+
+  charm {
+    name     = var.use_httprequest_lego_k8s_certificates ? "httprequest-lego-k8s" : "self-signed-certificates"
+    channel  = var.use_httprequest_lego_k8s_certificates ? var.httprequest_lego_k8s.channel : var.self_signed_ceritificates.channel
+    revision = var.use_httprequest_lego_k8s_certificates ? var.httprequest_lego_k8s.revision : var.self_signed_ceritificates.revision
+    base     = var.use_httprequest_lego_k8s_certificates ? var.httprequest_lego_k8s.base : var.self_signed_ceritificates.base
+  }
+
+  config      = var.use_httprequest_lego_k8s_certificates ? var.httprequest_lego_k8s.config : var.self_signed_ceritificates.config
+  constraints = var.use_httprequest_lego_k8s_certificates ? var.httprequest_lego_k8s.constraints : var.self_signed_ceritificates.constraints
+  units       = 1
+}
+
+resource "juju_integration" "public_ingress_certificates" {
+  model = var.model
+  application {
+    name     = juju_application.certificate_provider.name
+    endpoint = "certificates"
+  }
+  application {
+    name     = juju_application.public_ingress.name
+    endpoint = "certificates"
+  }
+}
+
+resource "null_resource" "wait_for_certs_settle" {
+  provisioner "local-exec" {
+    command = "juju wait-for application ${module.jenkins_k8s.app_name}; juju wait-for application ${juju_application.certificate_provider.name}"
+  }
+}
+
+
 resource "juju_integration" "jenkins_k8s_jenkins_agent_k8s_agent" {
   model = var.model
+
+  depends_on = [null_resource.wait_for_jenkins_up, null_resource.wait_for_certs_settle]
 
   application {
     name     = module.jenkins_k8s.app_name
--- terraform/product/variables.tf
+++ terraform/product/variables.tf
@@ -16,6 +16,7 @@ variable "jenkins_agent_k8s" {
     base        = optional(string, "ubuntu@24.04")
     units       = optional(number, 3)
   })
+  default = {}
 }
 
 variable "jenkins_k8s" {
@@ -27,4 +28,84 @@ variable "jenkins_k8s" {
     revision    = optional(number)
     base        = optional(string, "ubuntu@24.04")
   })
+  default = {}
+}
+
+variable "public_ingress" {
+  type = object({
+    app_name = optional(string, "public-traefik-k8s")
+    channel  = optional(string, "latest/edge")
+    config = optional(map(string), {
+      "enable_experimental_forward_auth" : "true",
+      "external_hostname" : ""
+    })
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@20.04")
+  })
+  default = {}
+}
+
+variable "agent_discovery_ingress" {
+  type = object({
+    app_name    = optional(string, "agent-discovery-traefik-k8s")
+    channel     = optional(string, "latest/edge")
+    config      = optional(map(string), {})
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@20.04")
+  })
+  default = {}
+}
+
+variable "oauth2_proxy" {
+  type = object({
+    app_name    = optional(string, "oauth2-proxy-k8s")
+    channel     = optional(string, "latest/edge")
+    config      = optional(map(string), {})
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@22.04")
+  })
+  default = {}
+}
+
+variable "httprequest_lego_k8s" {
+  type = object({
+    app_name = optional(string, "httprequest-lego-k8s")
+    channel  = optional(string, "latest/edge")
+    config = optional(map(string), {
+      "email" : "",
+      "httpreq_endpoint" : "",
+      "httpreq_http_timeout" : "180",
+      "httpreq_password" : "",
+      "httpreq_propagation_timeout" : "600"
+      "httpreq_username" : ""
+    })
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@24.04")
+  })
+  default = {}
+}
+
+variable "self_signed_ceritificates" {
+  type = object({
+    app_name    = optional(string, "self-signed-certificates")
+    channel     = optional(string, "1/stable")
+    config      = optional(map(string), {})
+    constraints = optional(string, "")
+    revision    = optional(number)
+    base        = optional(string, "ubuntu@24.04")
+  })
+  default = {}
+}
+
+variable "use_httprequest_lego_k8s_certificates" {
+  type        = bool
+  default     = false
+  description = <<EOT
+  Whether to use httprequest lego k8s to receive ceritificates.
+  Uses self-signed-certificates by default otherwise.
+  EOT
 }
