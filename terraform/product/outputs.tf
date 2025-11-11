35b3973a1c57f7d00371fcbef90ab6ac1414026e
Arturo Seijas <arturo.seijas@canonical.com>
2025-11-11 12:07:31 +0100

    Fix terraform product

--- terraform/product/outputs.tf
+++ terraform/product/outputs.tf
@@ -10,3 +10,31 @@ output "jenkins_agent_k8s_app_name" {
   description = "Name of the the deployed jenkins_agent_k8s application."
   value       = module.jenkins_agent_k8s.app_name
 }
+
+output "jenkins_k8s_requires" {
+  value = {
+    logging = "logging"
+  }
+}
+
+output "jenkins_k8s_provides" {
+  value = {
+    grafana_dashboard = "grafana-dashboard"
+    metrics_endpoint  = "metrics-endpoint"
+  }
+}
+
+output "public_ingress_requires" {
+  value = {
+    certificates = "certificates"
+    logging      = "logging"
+  }
+}
+
+output "public_ingress_provides" {
+  value = {
+    grafana_dashboard = "grafana-dashboard"
+    metrics_endpoint  = "metrics-endpoint"
+  }
+}
+
