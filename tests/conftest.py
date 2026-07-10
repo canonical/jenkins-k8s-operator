41bcdbffd3cb83ce61680f35e9e997da28cbf476
charlie4284 <yangsoo.yoon@canonical.com>
2026-07-10 14:26:30 +0800

    chore: revert options

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,13 +12,11 @@ def pytest_addoption(parser: pytest.Parser):
     Args:
         parser: pytest command line parser.
     """
-    # Guard against duplicate registration (e.g., when pytest-operator is installed).
-    for option in ("--charm-file", "--jenkins-image", "--kube-config", "--num-units"):
-        try:
-            parser.addoption(
-                option,
-                action="append" if option == "--charm-file" else "store",
-                default=[],
-            )
-        except ValueError:
-            pass
+    # The prebuilt charm file.
+    parser.addoption("--charm-file", action="append", default=[])
+    # The Jenkins image name:tag.
+    parser.addoption("--jenkins-image", action="store", default="")
+    # The path to kubernetes config.
+    parser.addoption("--kube-config", action="store", default="~/.kube/config")
+    # The number of jenkins agents to deploy and relate.
+    parser.addoption("--num-units", action="store", default="2")
